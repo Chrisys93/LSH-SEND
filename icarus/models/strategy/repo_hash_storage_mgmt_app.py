20a4caa5b0fc3ffcbec8c31707d6f2a9d64493ae
Chrisys93 <nicolaescu3@yahoo.com>
2022-03-18 10:29:43 +0000

    Multi-rate config MNIST CPU-usage Orchestration

--- icarus/models/strategy/repo_hash_storage_mgmt_app.py
+++ icarus/models/strategy/repo_hash_storage_mgmt_app.py
@@ -903,11 +903,11 @@ class HashRepoReuseStorApp(Strategy):
         #     if type(node) is int and self.view.model.avg_CPU_perc[node] > 0.7:
         #         self.trigger_node_CPU_update(curTime, 10)
 
-        if self.epoch_count >= self.epoch_ticks and type(node) is int:
-            if self.view.model.avg_CPU_perc[node] > 0.7:
-                updated_nodes = self.trigger_node_proc_reuse_update(curTime, 5)
-                updated_nodes += self.trigger_node_reuse_proc_update(curTime, 5)
-                self.controller.reset_update_CPU_perc(updated_nodes)
+        # if self.epoch_count >= self.epoch_ticks and type(node) is int:
+        #     if self.view.model.avg_CPU_perc[node] > 0.7:
+        #         updated_nodes = self.trigger_node_proc_reuse_update(curTime, 5)
+        #         updated_nodes += self.trigger_node_reuse_proc_update(curTime, 5)
+        #         self.controller.reset_update_CPU_perc(updated_nodes)
 
         # if self.epoch_count >= self.epoch_ticks and type(node) is int:
         #     self.trigger_node_proc_update(curTime, 20)
@@ -915,10 +915,10 @@ class HashRepoReuseStorApp(Strategy):
         # if type(self.epoch_count) is int and type(node) is int and self.view.model.avg_CPU_perc[node] > 0.7:
         #     self.trigger_node_proc_update(curTime, 20)
 
-        # if type(self.epoch_count) is int and type(node) is int and self.view.model.avg_CPU_perc[node] > 0.7 and self.epoch_count >= self.epoch_ticks:
-        #     updated_nodes = self.trigger_node_CPU_update(curTime, 20)
-        #     self.epoch_count = 0
-        #     self.controller.reset_update_CPU_perc(updated_nodes)
+        if type(self.epoch_count) is int and type(node) is int and self.view.model.avg_CPU_perc[node] > 0.7 and self.epoch_count >= self.epoch_ticks:
+            updated_nodes = self.trigger_node_CPU_update(curTime, 20)
+            self.epoch_count = 0
+            self.controller.reset_update_CPU_perc(updated_nodes)
 
         # if type(self.epoch_ticks) is int and type(node) is int and self.view.model.max_queue_delay[node] > 0.7:
         #     self.trigger_node_delay_update(curTime, 20)
